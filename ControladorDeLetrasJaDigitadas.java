5572bd638fff0916e2166bf51e00506f4ef1fc52
filipe mota <filipe.dmtm@puccampinas.edu.br>
2024-11-10 03:15:10 -0300

    Implementando mais alguns métodos obrigatórios da classe Controladora de letras já digitadas

--- ControladorDeLetrasJaDigitadas.java
+++ ControladorDeLetrasJaDigitadas.java
@@ -39,6 +39,7 @@ public class ControladorDeLetrasJaDigitadas implements Cloneable
         }
     }
     
+    @Override
     public String toString ()
     {
 		// retorna um String com TODAS as letras presentes em
@@ -54,7 +55,8 @@ public class ControladorDeLetrasJaDigitadas implements Cloneable
 
         return listaDigitadas.toString();
     }
-//
+
+    @Override
     public boolean equals (Object obj)
     {
        // verificar se this e obj s�o iguais
@@ -66,21 +68,32 @@ public class ControladorDeLetrasJaDigitadas implements Cloneable
         if(!(this.letrasJaDigitadas.equal(other.letrasJaDigitadas))) return false;
         return true;
     }
-//
-//    public int hashCode ()
-//    {
-//        // calcular e retornar o hashcode de this
-//    }
-//
-//    public ControladorDeLetrasJaDigitadas(
-//    ControladorDeLetrasJaDigitadas controladorDeLetrasJaDigitadas)
-//    throws Exception // construtor de c�pia
-//    {
-//        // copiar c.letrasJaDigitadas em this.letrasJaDigitadas
-//    }
-//
-//    public Object clone ()
-//    {
-//        // criar uma c�pia do this com o construtor de c�pia e retornar
-//    }
+
+    @Override
+    public int hashCode ()
+    {
+        // calcular e retornar o hashcode de this
+        int ret = 1;
+        ret = ret* 2 + String.valueOf(this.letrasJaDigitadas).hashCode();
+        return ret;
+    }
+
+   public ControladorDeLetrasJaDigitadas(ControladorDeLetrasJaDigitadas controladorDeLetrasJaDigitadas) throws Exception // construtor de c�pia
+    {
+        // copiar c.letrasJaDigitadas em this.letrasJaDigitadas
+        if(controladorDeLetrasJaDigitadas == null) throw new Exception("Controlador de letras já digitadas inválido");
+        this.letrasJaDigitadas = controladorDeLetrasJaDigitadas.letrasJaDigitadas
+    }
+
+    @Override
+    public Object clone ()
+    {
+        // criar uma c�pia do this com o construtor de c�pia e retornar
+        ControladorDeLetrasJaDigitadas other = null;
+        try{
+            other = new ControladorDeLetrasJaDigitadas(this);
+        }catch(Exception e){}
+
+        return other;
+    }
 }
