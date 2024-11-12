cdb6621776eab519b18b3a32b256074928db4537
Thiago Luiz Fossa <thiago.fossa433@gmail.com>
2024-11-12 08:28:17 -0300

    .

--- Main.java
+++ Main.java
@@ -7,6 +7,10 @@ public class Main {
             System.out.println("Tamanho da palavra: " + palavra.getTamanho());
             System.out.println("Quantidade de letras 'E': " + palavra.getQuantidade('E'));
             System.out.println("Palavra como string: " + palavra.toString());
+            System.out.println("Posição da primeira ocorrência de 'E': " + palavra.getPosicaoDaIezimaOcorrencia(0, 'E'));
+            System.out.println("Posição da segunda ocorrência de 'E': " + palavra.getPosicaoDaIezimaOcorrencia(1, 'E'));
+            System.out.println("Palavra é igual a 'EXEMPLO'? " + palavra.equals(new Palavra("EXEMPLO")));
+            System.out.println("Hash code da palavra: " + palavra.hashCode());
 
             // Testando a classe ControladorDeErros
             System.out.println("\n=== Testando ControladorDeErros ===");
@@ -15,6 +19,8 @@ public class Main {
             controladorDeErros.registreUmErro();
             System.out.println("Atingiu o máximo de erros? " + controladorDeErros.isAtingidoMaximoDeErros());
             System.out.println("Controlador de erros como string: " + controladorDeErros.toString());
+            System.out.println("Controlador de erros é igual a outro controlador com 3 erros? " + controladorDeErros.equals(new ControladorDeErros(3)));
+            System.out.println("Hash code do controlador de erros: " + controladorDeErros.hashCode());
 
             // Testando a classe ControladorDeLetrasJaDigitadas
             System.out.println("\n=== Testando ControladorDeLetrasJaDigitadas ===");
@@ -24,6 +30,8 @@ public class Main {
             System.out.println("Letra 'A' já foi digitada? " + letrasJaDigitadas.isJaDigitada('A'));
             System.out.println("Letra 'C' já foi digitada? " + letrasJaDigitadas.isJaDigitada('C'));
             System.out.println("Letras já digitadas: " + letrasJaDigitadas.toString());
+            System.out.println("Controlador de letras já digitadas é igual a outro controlador? " + letrasJaDigitadas.equals(new ControladorDeLetrasJaDigitadas()));
+            System.out.println("Hash code do controlador de letras já digitadas: " + letrasJaDigitadas.hashCode());
 
             // Testando a classe Tracinhos
             System.out.println("\n=== Testando Tracinhos ===");
@@ -31,6 +39,9 @@ public class Main {
             tracinhos.revele(0, 'E');
             tracinhos.revele(2, 'X');
             System.out.println("Estado dos tracinhos: " + tracinhos.toString());
+            System.out.println("Ainda há tracinhos? " + tracinhos.isAindaComTracinhos());
+            System.out.println("Tracinhos é igual a outro tracinhos? " + tracinhos.equals(new Tracinhos(palavra.getTamanho())));
+            System.out.println("Hash code dos tracinhos: " + tracinhos.hashCode());
 
             System.out.println("\nTodos os métodos foram testados com sucesso.");
         } catch (Exception e) {
